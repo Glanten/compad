3462a858a54e76ae33423faf3d9c72b971f94b7a
Glanten <tern.glanten@gmail.com>
2024-06-07 09:16:41 +0100

    Changed 'body' block name to 'main' to accurately reflect tag. Added custom 404 page.

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Error: 404 page
+@app.errorhandler(404)
+def notfound(e):
+    return render_template("404.html")
+
+# Main pages
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/credits")
+def credits():
+    return render_template("credits.html")
+
+@app.route("/system")
+def system():
+    return render_template("system.html")
+
+@app.route("/starmap")
+def starmap():
+    return render_template("starmap.html")
+
+@app.route("/compad")
+def compad():
+    return render_template("compad.html")
