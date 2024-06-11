2277a45ad18db1eda4c401866aefd73f90fd57a2
Glanten <tern.glanten@gmail.com>
2024-06-11 09:00:45 +0100

    Branch for SQLite instead of MongoDB

--- app.py
+++ app.py
@@ -1,50 +1,42 @@
-from flask import Flask, render_template
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 
-# +++ Database and MongoDB +++
-# MongoDB connection
-mongo_compad_uri = "mongodb+srv://compadadmin:Yu0tuPqQkPYyabr3@compadcluster00.gsqlhgd.mongodb.net/?retryWrites=true&w=majority&appName=CompadCluster00"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-# Create a new client and connect to the server
-client = MongoClient(mongo_compad_uri, server_api=ServerApi('1'))
+# +++ Database and SQL +++
+db = SQL("sqlite:///compad.db")
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Connection to database successful")
-except Exception as mongo_error_message:
-    print(mongo_error_message)
 
-# create variable for Database
-compad_db = client["compaddb"]
+# Can't remember what this does...?
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-# create variable for characters' credits Collection
-credits_collection = compad_db["credits"]
 
 # ++++++++++++++++++++++++++++++++++++++++++++++
 # +++ Flask and Webpages +++
-# Error: 404 page
-@app.errorhandler(404)
-def notfound(e):
-    return render_template("404.html")
 
 # Main pages
 @app.route("/")
+# @login_required
 def index():
     return render_template("index.html")
 
 @app.route("/credits")
 def credits():
-    # query database for each character's credits
-    gunner = credits_collection.find_one({"characterName": "gunner"})
-    cassidy = credits_collection.find_one({"characterName": "cassidy"})
-    glynn = credits_collection.find_one({"characterName": "glynn"})
-    holt = credits_collection.find_one({"characterName": "holt"})
-
-    return render_template("credits.html", gunner_balance=gunner["balance"], cassidy_balance=cassidy["balance"], holt_balance=holt["balance"], glynn_balance=glynn["balance"])
+    return render_template("credits.html")
 
 @app.route("/system")
 def system():
@@ -57,3 +49,8 @@ def starmap():
 @app.route("/compad")
 def compad():
     return render_template("compad.html")
+
+# Error: 404 page
+@app.errorhandler(404)
+def notfound(e):
+    return render_template("404.html")
