647aa982340f9f4a92f07058e0bef411973e988d
Glanten <tern.glanten@gmail.com>
2024-06-11 14:24:20 +0100

    Login/out working with SQLite database and test user. Needs better checking, UI, and error handling.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+
+from helpers import login_required
 # from pymongo.mongo_client import MongoClient
 # from pymongo.server_api import ServerApi
 
@@ -30,7 +32,7 @@ def after_request(response):
 
 # Main pages
 @app.route("/")
-# @login_required
+@login_required
 def index():
     return render_template("index.html")
 
@@ -50,7 +52,49 @@ def starmap():
 def compad():
     return render_template("compad.html")
 
-# Error: 404 page
+# Error page
 @app.errorhandler(404)
 def notfound(e):
-    return render_template("404.html")
+    return render_template("error.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """User login - requires username and password"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html")
+        
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html")
+
+        # Query database for username and password
+        submitted_user = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(submitted_user) != 1:
+            return render_template("error.html")
+
+        # Remember which user has logged in
+        session["user_id"] = submitted_user[0]["id"]
+
+        # If all went well, redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    # Forget any user_id
+    session.clear()
+    # Redirect user to login form
+    return redirect("/")
