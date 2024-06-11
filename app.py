25282d496d723b952c27701a49683758b95e4ce6
Glanten <tern.glanten@gmail.com>
2024-06-11 18:55:30 +0100

    Registration and user database (now includes admin field for users). Can now comment out registration.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
 # from pymongo.mongo_client import MongoClient
@@ -36,6 +37,89 @@ def after_request(response):
 def index():
     return render_template("index.html")
 
+# register a new user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register a new user (intended for initial setup only)"""
+    if request.method == "POST":
+        # if user hits register button on form...
+        # create lower-case version of username
+        submitted_username = request.form.get("username").lower()
+
+        # query database for username match
+        existing_name = db.execute(
+            "SELECT * FROM users WHERE username = ?", submitted_username
+        )
+        if existing_name:
+            # if <existing_name> returns anything, a record must already exist
+            return render_template("error.html")
+        # ensure a username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html")
+        # ensure a password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html")
+        # ensure password and confirmation match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("error.html")
+        
+        # register the user
+        else:
+            hashed_password = generate_password_hash(
+                request.form.get("password"), method='scrypt', salt_length=16)
+            db.execute("INSERT INTO users (username, hash) VALUES(?, ?)",
+                       submitted_username, hashed_password)
+            return render_template("login.html")
+        
+    else:
+    # user arrived by GET (i.e. typed in URL or via link) instead of POST, display registration page
+    # send user to appropriate web page
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """User login - requires username and password"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html")
+        
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template("error.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # If all went well, redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    """User gets logged out"""
+    # Forget any user_id
+    session.clear()
+    # Redirect user to login form
+    return redirect("/")
+
 @app.route("/credits")
 def credits():
     return render_template("credits.html")
@@ -56,45 +140,3 @@ def compad():
 @app.errorhandler(404)
 def notfound(e):
     return render_template("error.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """User login - requires username and password"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return render_template("error.html")
-        
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return render_template("error.html")
-
-        # Query database for username and password
-        submitted_user = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(submitted_user) != 1:
-            return render_template("error.html")
-
-        # Remember which user has logged in
-        session["user_id"] = submitted_user[0]["id"]
-
-        # If all went well, redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-    
-@app.route("/logout")
-def logout():
-    """Log user out"""
-    # Forget any user_id
-    session.clear()
-    # Redirect user to login form
-    return redirect("/")
